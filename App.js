ac45fcdc4cdca1a501770de129a266d2e3ef6618
CreditableX <creditablecreates@gmail.com>
2024-06-06 23:26:13 +0800

    basic hardcoded auth

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 import StackNavigator from './StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './hooks/useAuth';
 
 
 export default function App() {
   const tw = useTailwind();
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
     </NavigationContainer>
 
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/core';
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button title="Go to Chat Screen"
+      onPress={() => navigation.navigate("Chat")}
+      />
     </View>
   )
 }
