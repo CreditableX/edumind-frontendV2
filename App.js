79d785be1ceb9e340e2e750169de1a0c01ac9f80
CreditableX <creditablecreates@gmail.com>
2024-07-19 15:55:30 +0800

    cleanup file

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { useTailwind } from 'tailwind-rn';
 import StackNavigator from './StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainProvider } from './hooks/mainProvider';
@@ -16,7 +13,5 @@ export default function App() {
       </MainProvider>
     </NavigationContainer>
     </PaperProvider>
-
-
   );
 }
